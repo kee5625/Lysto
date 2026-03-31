4d2f69b2a56c4c958ea4bba2cc8162708379aa1d
Karthik R <karthikeya.rachamolla@gmail.com>
2026-03-31 23:38:21 +0530

    adding health route to main.py

--- server/main.py
+++ server/main.py
@@ -11,3 +11,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/health")
+def get_health():
+    return {"status":"You're good buddy"}
